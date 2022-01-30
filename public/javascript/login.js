c27979cc8c50aa7611b85cf0324b09034b743302
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-30 16:29:33 -0500

    logic for developer

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,1 +1,68 @@
-// function for signup and login buttons on login.handlebars
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+    
+    const btn = document.querySelector('#btn');        
+    const rectangleButtons = document.querySelectorAll('input[name="size"]');
+    btn.addEventListener("click", () => {
+        let account_type;
+        for (const rectangleButton of rectangleButton) {
+            if (rectangleButton.checked) {
+                account_type = rectangleButton.value;
+                break;
+            }
+        }
+        // show the output:
+        output.innerText = account_type ? `You selected ${account_type}` : `You haven't selected account `;
+    });
+
+  
+    if (email && password && account) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
