9d348b7de0138894a7f81754612b9cc90870b1b0
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2022-01-31 20:50:37 -0500

    finished routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
             'service_description',
             'budget',
             'user_id'
-            //sequelize data
+            //[sequelize.literal('(SELECT COUNT(*) FROM user WHERE user.id')]
         ],
         include: [
             {
