cc6f8bc4da74d659fa84aee39d1708d4888addbf
Andi Conner <andiconner@icloud.com>
2022-02-02 11:02:52 -0500

    service_type issue

--- public/javascript/add-service.js
+++ public/javascript/add-service.js
@@ -1,15 +1,22 @@
 // function happening on the member-dashboard 
 //async function newFormHandler(event) for this form:  OK AC
 //<form class="new-service-form"> ok AC
+let serviceTypeData = ''
 
+const serviceTypeForm = document.getElementById('service-type')
+  serviceTypeForm.addEventListener("change", event => {
+    const {value} = event.target
+    serviceTypeData = value
+    console.log(serviceTypeData)
+  })
 async function newFormHandler(event) {
     event.preventDefault();
   
-    const service_type = document.querySelector('input[name="service-type"]').value;
+    const service_type = serviceTypeData;
     const service_title = document.querySelector('input[name="service-title"]').value;
     const service_description = document.querySelector('input[name="service-description"]').value;
     const budget = document.querySelector('input[name="budget"]').value;
-
+    console.log(service_type)
     const response = await fetch(`/api/service`, {
       method: 'POST',
       body: JSON.stringify({
@@ -24,7 +31,7 @@ async function newFormHandler(event) {
     });
   
     if (response.ok) {
-     document.location.replace('/member');
+     //document.location.replace('/member');
     } else {
       alert(response.statusText);
     }
