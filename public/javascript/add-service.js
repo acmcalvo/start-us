524ae12996bfd43f40ad0e00de78fdb4bcb9f49c
Andi Conner <andiconner@icloud.com>
2022-02-01 23:57:58 -0500

    WIP

--- public/javascript/add-service.js
+++ public/javascript/add-service.js
@@ -10,7 +10,7 @@ async function newFormHandler(event) {
     const service_description = document.querySelector('input[name="service-description"]').value;
     const budget = document.querySelector('input[name="budget"]').value;
     alert(service_type+ service_title+ service_description+ budget)
-    const response = await fetch(`/api/services`, {
+    const response = await fetch(`/api/service`, {
       method: 'POST',
       body: JSON.stringify({
         service_type,
