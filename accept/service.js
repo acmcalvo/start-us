03d10170d4f1766f1dd2250a7be3ec09328c6d65
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2024-09-08 10:46:46 -0400

    Update vercl conection

--- accept/service.js
+++ accept/service.js
@@ -1,34 +1,54 @@
 define(['angular', 'toastr', 'storage'], function (angular, toastr, storage) {
     'use strict';
-    return angular.module('ess.services', []).service('api',function ($http) {
+    return angular.module('ess.services', []).service('api', function ($http) {
         var session = false;
+
         return {
             request: function (url, formData) {
-                $('#loading').show();
+                // Show loading spinner using AngularJS if possible
+                angular.element('#loading').show();
+
                 return $http({
                     method: 'POST',
                     url: url,
-                    data: $.param(formData),
+                    data: $.param(formData), // If you really need to use jQuery for parameter formatting
                     headers: {'Content-Type': 'application/x-www-form-urlencoded'}
                 }).then(function (response) {
-                        var data = response.data;
-                        if (data.errors) {
-                            if (data.errors.length > 0) {
-                                if (data.errors[0].indexOf('Invalid Session') !== -1) {
-                                    // fix for multiple notifications
-                                    if(!session) {
-                                        toastr.error('Invalid session! Please login again!');
-                                        location.hash = 'login';
-                                        session = true;
-                                    }
-                                } else {
-                                    $.each(data.errors, function (key, val) {
-                                        toastr.error(val);
-                                    });
+                    var data = response.data;
+
+                    // Check if there are errors in the response
+                    if (data.errors) {
+                        if (data.errors.length > 0) {
+                            if (data.errors[0].indexOf('Invalid Session') !== -1) {
+                                // Prevent multiple notifications for invalid session
+                                if (!session) {
+                                    toastr.error('Invalid session! Please login again!');
+                                    session = true;
+                                    location.hash = 'login';
                                 }
+                            } else {
+                                // Show all errors using toastr
+                                angular.forEach(data.errors, function (val) {
+                                    toastr.error(val);
+                                });
                             }
-                        } else {
-                            session = false;
                         }
-                        $('#loading').delay(500).fadeOut();
-                        return data
+                    } else {
+                        session = false; // Reset session flag if no errors
+                    }
+
+                    // Hide loading spinner after a delay
+                    angular.element('#loading').delay(500).fadeOut();
+
+                    return data; // Ensure data is returned to the calling function
+                }).catch(function (error) {
+                    // Handle HTTP errors
+                    toastr.error('An error occurred during the request.');
+                    angular.element('#loading').fadeOut();
+                    console.error(error);
+                    return $q.reject(error); // Properly handle promise rejection
+                });
+            }
+        };
+    });
+});
